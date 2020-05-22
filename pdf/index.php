43529a84774d05937262ba94b701127b36516b3c
jesussantrich <losdelsursdvsf841@gmail.com>
2020-05-22 18:58:39 -0500

    cambios de diseño

--- pdf/index.php
+++ pdf/index.php
@@ -3,10 +3,12 @@
 	//plantilla html
 	require_once('vendor/autoload.php');
 	//codigo css plantilla
-
 	require_once('plantillas/page.php');
-
+	//pedir los datos del crud para imprimirlos en el pdf
 	require_once('../modelos/crud_basicos.php');
+	
+
+	
 
 	
 	
@@ -20,13 +22,13 @@
     
 
 	$css = file_get_contents('plantillas/style.css');
+	
 
 	$mpdf = new \Mpdf\Mpdf([
 
 	]);
-
+	
 	$plantilla = getPlantilla($result,$result2);
-
 	$mpdf->writeHtml($css, \Mpdf\HTMLParserMode::HEADER_CSS);
 	$mpdf->writeHtml($plantilla, \Mpdf\HTMLParserMode::HTML_BODY);
 	
--- pdf/plantillas/page.php
+++ pdf/plantillas/page.php
@@ -1,5 +1,7 @@
+
 <?php 
 require_once("../modelos/crud_basicos.php");
+
 
 function getPlantilla($result, $result2){
 
@@ -15,15 +17,15 @@ $plantilla = '<body>
     $res=new datosBasicos();
           $result=$res->mostrar_datos_basicos();
       for($i=0; $i <count($result) ; $i++) {
-          $plantilla .='<div><span>Fecha</span>'.$result[$i]["fecha"].'</div>
-      <div><span>Hora</span>'.$result[$i]["hora"].'</div>
-      <div><span>Diligenciador</span>'.$result[$i]["diligenciador"].'</div>
-      <div><span>Comprador</span>'.$result[$i]["comprador"].'</div>
-      <div><span>Empresa</span>'.$result[$i]["empresa"].'</div>
-      <div><span>Ciudad</span>'.$result[$i]["ciudad"].'</div>
-      <div><span>Direccion</span>'.$result[$i]["direccion"].'</div>
-      <div><span>Correo</span><a href="mailto:john@example.com">'.$result[$i]["correo"].'</a></div>
-      <div><span>Telefono</span>'.$result[$i]["telefono"].'</div>';
+          $plantilla .='<div class="container"><span>Fecha : </span>'.$result[$i]["fecha"].'</div>
+      <div><span>Hora : </span>'.$result[$i]["hora"].'</div>
+      <div><span>Diligenciador : </span>'.$result[$i]["diligenciador"].'</div>
+      <div><span>Comprador : </span>'.$result[$i]["comprador"].'</div>
+      <div><span>Empresa : </span>'.$result[$i]["empresa"].'</div>
+      <div><span>Ciudad : </span>'.$result[$i]["ciudad"].'</div>
+      <div><span>Direccion :</span>'.$result[$i]["direccion"].'</div>
+      <div><span>Correo : </span><a href="mailto:john@example.com">'.$result[$i]["correo"].'</a></div>
+      <div><span>Telefono : </span>'.$result[$i]["telefono"].'</div>';
       }
    $plantilla .='</div>
       
@@ -39,13 +41,44 @@ $plantilla = '<body>
           <th>ADD</th>
           <th>Tipo de lente</th>
           <th>ESF</th>
+        </tr>
+      </thead>
+      <tbody>';
+      
+      $res2=new datosBasicos();
+        $result2=$res2->mostrar_datos_especificos();  
+      for ($i=0; $i <count($result2 ) ; $i++) { 
+        
+      
+        
+      
+       $plantilla .='<tr>
+          <td>'.$result2[$i]["esf1"].'</td>
+          <td class="">'.$result2[$i]["cil1"].'</td>
+          <td class="">'.$result2[$i]["eje1"].'</td>
+          <td class="">'.$result2[$i]["add1"].'</td>
+          <td class="">'.$result2[$i]["tipo_lente1"].'</td>
+          <td class="">'.$result2[$i]["esf2"].'</td>
+          
+
+        </tr>';
+        }
+      $plantilla .='</tbody>
+    </table>
+    <br>
+    <br>
+    <br>
+
+    <table>
+      <thead>
+        <tr>
+          <th>ESF</th>
           <th>CIL</th>
           <th>EJE</th>
           <th>ADD</th>
           <th>Tipo de lente</th>
-          <th>Filtro</th>
-          <th>Montadura</th>
-          <th>Tipo de talla</th>
+          <th>ESF</th>
+          
 
 
         </tr>
@@ -60,19 +93,13 @@ $plantilla = '<body>
         
       
        $plantilla .='<tr>
-          <p>'.$result2[$i]["esf1"].'</p>
+          <td>'.$result2[$i]["esf1"].'</td>
           <td class="">'.$result2[$i]["cil1"].'</td>
           <td class="">'.$result2[$i]["eje1"].'</td>
           <td class="">'.$result2[$i]["add1"].'</td>
           <td class="">'.$result2[$i]["tipo_lente1"].'</td>
           <td class="">'.$result2[$i]["esf2"].'</td>
-          <td class="">'.$result2[$i]["cil2"].'</td>
-          <td class="">'.$result2[$i]["eje2"].'</td>
-          <td class="">'.$result2[$i]["add2"].'</td>
-          <td class="">'.$result2[$i]["tipo_lente2"].'</td>
-          <td class="">'.$result2[$i]["filtro"].'</td>
-          <td class="">'.$result2[$i]["montadura"].'</td>
-          <td class="">'.$result2[$i]["tipo_talla"].'</td>
+          
 
         </tr>';
         }
